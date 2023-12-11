60ebde35de20716cbf2d1c41b7c5e2c3a56070d0
Subhasis Ghosh <110963431+Subhasis0520@users.noreply.github.com>
2023-12-11 18:54:34 +0530

    Delete 006-DataJPA directory

--- 006-DataJPA/src/main/java/com/org/dao/CouresDao.java
+++
@@ -1,9 +0,0 @@
-package com.org.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.org.entity.Course;
-
-public interface CouresDao extends JpaRepository<Course, Integer> {
-
-}
