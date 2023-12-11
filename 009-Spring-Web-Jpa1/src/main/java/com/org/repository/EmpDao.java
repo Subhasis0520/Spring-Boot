aa0c59c5cfb4018089111abe1140082059b05466
Subhasis Ghosh <110963431+Subhasis0520@users.noreply.github.com>
2023-12-11 18:55:17 +0530

    Delete 009-Spring-Web-Jpa1 directory

--- 009-Spring-Web-Jpa1/src/main/java/com/org/repository/EmpDao.java
+++
@@ -1,9 +0,0 @@
-package com.org.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.org.entity.Employee;
-
-public interface EmpDao extends JpaRepository<Employee, Integer>{
-
-}
